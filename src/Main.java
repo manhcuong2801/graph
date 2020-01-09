d98077bf5914466a187da3301b037bd4b62a40f5
manhcuong2801 <le.manhcuong.hd1996.1@gmail.com>
2020-01-09 11:50:40 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+     int [][] b = {
+         {0,1,1,0,0,0},   
+         {1,0,0,1,0,0},   
+         {1,0,0,0,0,0},   
+         {0,1,0,0,0,0},   
+         {0,0,0,0,0,1},   
+         {0,0,0,0,1,0}   
+     };
+     Graph g = new Graph();
+     g.setData(b);
+     System.out.println("1. Display adjancy matrix ");
+     g.dispAdj();
+     System.out.println();
+        
+     System.out.println("2. Test breadth-first traversal ");
+     g.breadth(0);
+     System.out.println();
+
+     System.out.println(".3 Test depth-first traversal ");
+     g.depth(0);
+     System.out.println();
+    }
+    
+}
